ed1ff02dd51b63bed8973aa356720f046594d67e
jenkins_ci <jenkins_ci@limxdynamics.com>
2024-03-27 10:14:31 +0800

    1.0.0.20240327100016

---
+++ python3/win/limxsdk/datatypes/RobotState.py
@@ -0,0 +1,12 @@
+﻿import sys
+
+class RobotState(object):
+    __slots__ = ['stamp','imu_acc','imu_gyro','imu_quat','tau','q','dq']
+    def __init__(self):
+        self.stamp = 0
+        self.imu_acc = [0. for x in range(0, 3)]
+        self.imu_gyro = [0. for x in range(0, 3)]
+        self.imu_quat = [0. for x in range(0, 4)]
+        self.tau = []
+        self.q = []
+        self.dq = []
